1c1b37e4e3726fee607d4ae29b8c764b310fbb85
Muhammed Midlaj <muhammedmidlaj711@gmail.com>
2024-08-08 12:13:06 +0530

    navbar and banner

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner-buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='description'>In gragphic designing the lorem ipsum maintaining a big place for the situation.</h1>
+        </div>
+        <div className="fade">
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Banner
